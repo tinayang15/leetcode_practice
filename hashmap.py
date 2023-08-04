7521e783024c3a20e2726c4da3e91c85fb76820b
Tina Yang <Tinayangers@gmail.com>
2023-08-03 20:20:51 -0500

    added hashmap key and value

--- hashmap.py
+++ hashmap.py
@@ -2,3 +2,12 @@
 city_map = {}
 # OR
 city_maps = dict()
+
+cities = ["Calgary", "Vancouver", "Toronto"]
+
+# city_map["Canada"].append(cities)
+# # KeyError: 'Canada' - you get this error because no intialization how to fix is below
+
+city_map["Canada"] = []
+city_map["Canada"] += cities
+{'Canada': ["Calgary", "Vancouver", "Toronto"]}
