56e98ce464825af79603b6180c8418e8faa30f2c
Tina Yang <Tinayangers@gmail.com>
2023-08-22 22:27:53 -0500

    Added solution to cycle linked list hackerrank

---
+++ cyclelinkedlist.py
@@ -0,0 +1,9 @@
+def has_cycle(head):
+    slow = fast = head
+
+    while fast != None and fast.next != None:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
